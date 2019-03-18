a74aca8317be964ff1efc9733e9885d209488b09
Arlen Nascimento <arlen.nascimento@neemu.com>
2019-03-18 11:05:45 -0400

    increasing token expiration time to 24h

--- app/controllers/auth.js
+++ app/controllers/auth.js
@@ -9,7 +9,7 @@ module.exports = {
             .then(userInfo => {
                 if(userInfo) {
                     if (userInfo.validPassword(req.body.password)) {
-                        const token = jwt.sign({ id: userInfo.id}, config.auth.secretKey, { expiresIn: '1h' });
+                        const token = jwt.sign({ id: userInfo.id}, config.auth.secretKey, { expiresIn: '24h' });
                         res.json({ status:"success", message: "user found!!!", data: { user: userInfo, token: token }});
                     }else{
                         res.json({ status:"error", message: "Invalid email/password!!!", data:null });
