8a3c5fa2eb42b70b7f61bb4bdb0beafd5822da13
Arlen Nascimento <arlen.nascimento@neemu.com>
2019-03-18 13:25:18 -0400

    prettier index

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-const router = require('./app/router/');
+const router = require("./app/router/");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.use('/api', router);
+app.use("/api", router);
 
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
   if (err.isBoom) {
-    console.log(req.body)
     return res.status(err.output.statusCode).json(err.output.payload);
   }
 });
 
 app.listen(3000, () => {
-    console.log('Node server listening on port 3000');
+  console.log("Node server listening on port 3000");
 });
