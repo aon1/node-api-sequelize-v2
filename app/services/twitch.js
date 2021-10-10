9d43c3a0090aefb80c2d70ad4278865bfe978cd1
Arlen Nascimento <arlen.nascimento@gmail.com>
2021-10-10 14:35:28 +0200

    Add twitch as a service

---
+++ app/services/twitch.js
@@ -0,0 +1,19 @@
+const { StaticAuthProvider } = require('@twurple/auth')
+const { ApiClient } = require('@twurple/api')
+const config = require('../../config/config')
+
+const clientId = config.TWITCH_CLIENT_ID
+const accessToken = config.TWITCH_ACCESS_TOKEN
+
+const authProvider = new StaticAuthProvider(clientId, accessToken)
+const apiClient = new ApiClient({ authProvider })
+
+module.exports = {
+  getTopGames (filter) {
+    return apiClient.games.getTopGames(filter)
+  },
+
+  getStreams (filter) {
+    return apiClient.streams.getStreams(filter)
+  }
+}
