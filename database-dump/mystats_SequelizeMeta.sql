65715be47aa217704d445a6187c6d93e1d46403f
Arlen Nascimento <arlen.nascimento@gmail.com>
2021-10-14 17:02:11 +0200

    Add database dump

---
+++ database-dump/mystats_SequelizeMeta.sql
@@ -0,0 +1,7 @@
+insert into mystats.SequelizeMeta (name)
+values  ('20211001162824-create-streamer.js'),
+        ('20211001164329-create-game.js'),
+        ('20211002083008-create-stream.js'),
+        ('20211002084238-create-viewer.js'),
+        ('20211002084600-create-follower.js'),
+        ('20211013162135-add-deletedAt-to-streamer.js');
