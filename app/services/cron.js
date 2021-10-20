a81ee56e0fae05efb6309794ce3da9a1e5d9a73a
Arlen Nascimento <arlen.nascimento@gmail.com>
2021-10-20 18:38:26 +0200

    Add youtube jobs

--- app/services/cron.js
+++ app/services/cron.js
@@ -1,18 +1,29 @@
 const cron = require('node-cron')
-const controller = require('../controllers/twtich')
+const twitch = require('../controllers/twtich')
+const youtube = require('../controllers/youtube')
 const config = require('../../config/config')
 const { logger } = require('../services/logger')
 
 module.exports = {
   schedule () {
-    cron.schedule(config.CRON_EXPRESSION_UPDATE_STREAMS, () => {
-      logger.info('running fetchStreams job')
-      controller.fetchStreamsJob()
+    cron.schedule(config.CRON_EXPRESSION_UPDATE_STREAMS_TWITCH, () => {
+      logger.info('running twitch fetchStreams job')
+      twitch.fetchStreamsJob()
     })
 
-    cron.schedule(config.CRON_EXPRESSION_CHECK_STREAM_IS_FINISHED, () => {
-      logger.info('running streamHasFinishedJob job')
-      controller.streamHasFinishedJob()
+    cron.schedule(config.CRON_EXPRESSION_CHECK_STREAM_IS_FINISHED_TWITCH, () => {
+      logger.info('running twitch streamHasFinishedJob job')
+      twitch.streamHasFinishedJob()
+    })
+
+    cron.schedule(config.CRON_EXPRESSION_UPDATE_STREAMS_YOUTUBE, () => {
+      logger.info('running youtube currentStreams job')
+      youtube.fetchCurrentStreams()
+    })
+
+    cron.schedule(config.CRON_EXPRESSION_CHECK_STREAM_IS_FINISHED_YOUTUBE, () => {
+      logger.info('running youtube fetchStreams job')
+      youtube.fetchStreamsJob()
     })
   }
 }
