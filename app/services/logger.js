15219603418e156ae3f2ea6e5ed398cea7f5f8d7
Arlen Nascimento <arlen.nascimento@gmail.com>
2021-10-23 18:28:49 +0200

    Add timestamp to logger

--- app/services/logger.js
+++ app/services/logger.js
@@ -1,12 +1,24 @@
 const winston = require('winston')
+const { format } = require('winston')
+
+const myFormat = format.printf(({ level, message, label, timestamp }) => {
+  return `${timestamp} [${label}] ${level}: ${message}`
+})
 
 const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.json(),
-  defaultMeta: { service: 'twitch-youtube-stats' },
   transports: [
-    new winston.transports.Console()
-  ]
+    new winston.transports.Console({
+      level: 'info',
+      timestamp: true,
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        myFormat,
+        winston.format.json()
+      )
+    })
+  ],
+  level: 'info',
+  defaultMeta: { service: 'twitch-youtube-stats' }
 })
 
 module.exports = { logger }
--- index.js
+++ index.js
@@ -5,16 +5,25 @@ const app = express()
 const router = require('./app/router/')
 const port = require('./config/config')
 const scheduler = require('./app/services/cron')
+const { format } = require('winston')
+
+const myFormat = format.printf(({ level, message, label, timestamp }) => {
+  return `${timestamp} [${label}] ${level}: ${message}`
+})
 
 app.use(expressWinston.logger({
   transports: [
-    new winston.transports.Console()
+    new winston.transports.Console({
+      level: 'info',
+      timestamp: true,
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        myFormat,
+        winston.format.json()
+      )
+    })
   ],
-  format: winston.format.combine(
-    winston.format.json()
-  ),
   meta: false,
-  msg: 'HTTP  ',
   expressFormat: true,
   colorize: false,
   ignoreRoute: function (req, res) { return false }
